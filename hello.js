6c6ddd0eade9815fe44cc92b4837e7fbe14003e4
Eric Rozell <erozell@outlook.com>
2018-01-02 09:56:18 -0500

    WIP

--- hello.js
+++ hello.js
@@ -3,6 +3,10 @@ const Version = "2.0"
 console.log(`Thank you for using Hello ${Version}\n`)
 
 class Hello {
+  constructor(name) {
+    this._name = name
+  }
+
   greet() {
     console.log(`Hello at ${new Date().toLocaleTimeString()}`)
   }
