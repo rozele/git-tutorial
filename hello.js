3c7b023fce210ac0a5346f91a39bab280c33a6d9
Eric Rozell <erozell@outlook.com>
2018-01-02 15:02:18 -0500

    Adds empty default constructor to Hello class

--- hello.js
+++ hello.js
@@ -3,6 +3,10 @@ const Version = "2.0"
 console.log(`Thank you for using Hello ${Version}\n`)
 
 class Hello {
+  constructor() {
+
+  }
+  
   greet() {
     console.log(`Hello at ${new Date().toLocaleTimeString()}`)
   }
